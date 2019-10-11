c79e89e3eb3836cbbf24b354e5884bb60011a943
ysung327 <ysung327@naver.com>
2019-10-12 08:58:32 +0900

    initial commit

---
+++ Server/AbocadoProject/vacations/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from vacations.models import Vacation, Annual, Reward, Prize, Petition, Consolation
+
+admin.site.register(Vacation)
+admin.site.register(Annual)
+admin.site.register(Reward)
+admin.site.register(Consolation)
+admin.site.register(Prize)
+admin.site.register(Petition)
---
+++ Server/AbocadoProject/vacations/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+class Vacation(models.Model):
+    #User = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+    def __str__(self):
+        return self.title
+
+
+class Annual(models.Model):
+    vacation = models.ForeignKey(Vacation, on_delete=models.CASCADE)
+    day = models.IntegerField()
+
+    def __str__(self):
+        return "연가 : %d"   %self.day
+
+class Reward(models.Model): #보상
+    vacation = models.ForeignKey(Vacation, on_delete=models.CASCADE)
+    day = models.IntegerField()
+    title = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return "보상 %s : %d 일" %(self.title, self.day)
+
+class Consolation(models.Model): #위로
+    vacation = models.ForeignKey(Vacation, on_delete=models.CASCADE)
+    day = models.IntegerField()
+    title = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return "위로 %s : %d 일" %(self.title, self.day)
+
+class Prize(models.Model): #포상
+    vacation = models.ForeignKey(Vacation, on_delete=models.CASCADE)
+    day = models.IntegerField()
+    title = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return "포상 %s : %d 일" %(self.title, self.day)
+
+class Petition(models.Model):
+    vacation = models.ForeignKey(Vacation, on_delete=models.CASCADE)
+    day = models.IntegerField()
+    title = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return "청원 %s : %d 일" %(self.title, self.day)
