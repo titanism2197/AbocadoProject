556445aba462552d316197c8cf7e2752a7505537
ysung327 <ysung327@naver.com>
2019-11-05 16:58:18 +0900

    work 2019-11-05 customize vacation card

--- Client/AbocadoProject/components/VacationItem.js
+++ Client/AbocadoProject/components/VacationItem.js
@@ -5,23 +5,65 @@ import { Card } from 'react-native-elements'
 export default class VacationItem extends Component {
   render() {
     return (
-      <Card style={styles.item}>
-        <Text style={styles.content}>{this.props.days} 일</Text>
-        <Text style={styles.content}>{this.props.dday} 일</Text>
-        <Text style={styles.content}>출발 : {this.props.start_date}</Text>
-        <Text style={styles.content}>복귀 : {this.props.end_date}</Text>
-      </Card>
+      <View style={styles.container}>
+        <Card style={styles.item}>
+          <View style={styles.day}>
+            <Text style={styles.content}>{this.props.days} 일</Text>
+          </View>
+          <View style={styles.dday}>
+            <Text style={styles.content}>D-{this.props.dday}</Text>
+          </View>
+          <View style={styles.info}>
+            <View style={styles.date}>
+              <Text style={styles.text}>출발</Text>
+              <Text style={styles.text}>{this.props.start_date}</Text>
+            </View>
+            <View style={styles.date}>
+              <Text style={styles.text}>복귀</Text>
+              <Text style={styles.text}>{this.props.end_date}</Text>
+            </View>
+          </View>
+        </Card>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  container:{
+    width: 350,
+  },
+
   item: {
     flex: 1,
-    padding: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     backgroundColor: 'white',
   },
+
+  day:{
+    alignItems: 'center',
+  },
+
+  dday:{
+    alignItems: 'center',
+  },
+
   content: {
     fontSize: 30,
-  }
+  },
+
+  info: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  
+  date: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  text: {
+    fontSize: 17,
+  },
 })
--- Client/AbocadoProject/components/VacationList.js
+++ Client/AbocadoProject/components/VacationList.js
@@ -3,60 +3,69 @@ import { StyleSheet, FlatList, View } from 'react-native';
 import VacationItem from './VacationItem';
 
 export default class VacationList extends Component {
- constructor(props) {
-    super(props);
-    this.state  = {
-      loading: false,
-      data: [],
+  constructor(props) {
+      super(props);
+      this.state  = {
+        loading: false,
+        data: [],
+      }
+    }
+
+    componentDidMount() {
+      this.fetchDataFromApi();
+    }
+
+    fetchDataFromApi = ()  => {
+      const url = "http://testabocado.ml:8000/vacations/";
+
+      this.setState({ loading: true });
+
+      fetch(url)
+        .then(res => res.json())
+        .then(res => {
+          console.log(res)
+          console.log(typeof(res.start_date))
+          this.setState({
+            data: res,
+          });
+        })
+    };
+
+    _renderItem = ({item}) => {
+      return (
+      <VacationItem 
+        dday={this._getDday(item.start_date)}
+        days={item.day}
+        start_date={item.start_date}
+        end_date={item.end_date}
+      />)
+    };
+
+  _getDday = (start_date) => {
+    var now = new Date()
+    var dday = new Date(start_date)
+    var gap = now.getTime() - dday.getTime()
+    var result = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1
+    return result
+  };
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.data}
+            renderItem={this._renderItem}
+            horizontal='true'
+          />
+        </View>
+      );
     }
   }
 
-  componentDidMount() {
-    this.fetchDataFromApi();
-  }
-
-  fetchDataFromApi = ()  => {
-    const url = "http://testabocado.ml:8000/vacations/";
-
-    this.setState({ loading: true });
-
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        console.log(res)
-        console.log(typeof(res.start_date))
-        this.setState({
-          data: res,
-        });
-      })
-  };
-
-  _renderItem = ({item}) => {
-    return (
-    <VacationItem 
-      dday={this._getDday(item.start_date)}
-      days={item.day}
-      start_date={item.start_date}
-      end_date={item.end_date}
-    />)
-  };
-
- _getDday = (start_date) => {
-   var now = new Date()
-   var dday = new Date(start_date)
-   var gap = now.getTime() - dday.getTime()
-   var result = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1
-   return result
- };
-
-  render() {
-    return (
-      <View>
-        <FlatList
-          data={this.state.data}
-          renderItem={this._renderItem}
-        />
-      </View>
-    );
-  }
-}
+const styles = StyleSheet.create({
+  container:{
+    alignItems: 'center',
+    height: 225,
+    marginTop: 50,
+  },
+})
