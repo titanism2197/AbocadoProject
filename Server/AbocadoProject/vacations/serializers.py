0cd3543d96800d64de4ad3c58dae732f35ef491e
ysung327 <ysung327@naver.com>
2019-11-06 14:01:46 +0900

    work 2019-11-06 02

--- Server/AbocadoProject/vacations/serializers.py
+++ Server/AbocadoProject/vacations/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
 from vacations.models import Vacation
 
+
+
+class AnnualListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        data = {
+            "day": value.day
+        }
+        return data
+
+class ListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        data = {
+            "day": value.day,
+            "title": value.title
+        }
+        return data
+        
 class VacationSerializer(serializers.ModelSerializer):
-    anuual = serializers.StringRelatedField(many=True)
-    reward = serializers.StringRelatedField(many=True)
-    consolation = serializers.StringRelatedField(many=True)
-    prize = serializers.StringRelatedField(many=True)
-    petition = serializers.StringRelatedField(many=True)
+    annual = AnnualListingField(many=True, read_only=True)
+    reward = ListingField(many=True, read_only=True)
+    consolation = ListingField(many=True, read_only=True)
+    prize = ListingField(many=True, read_only=True)
+    petition = ListingField(many=True, read_only=True)
     class Meta:
         model = Vacation
-        fields = ['id', 'title', 'start_date', 'end_date', 'day', 
-                'anuual', 'reward', 'consolation', 'prize', 'petition']
+        fields = ['id', 'title', 'start_date', 'end_date', 'day', 'annual', 'reward', 'consolation', 'prize', 'petition']
