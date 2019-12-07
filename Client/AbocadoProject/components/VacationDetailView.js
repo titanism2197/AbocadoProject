030a1e8a1c61e30d428b9404d1ce0ae1d452847c
ysung327 <ysung327@naver.com>
2019-12-07 12:52:19 +0900

    work 2019-12-07-2

--- Client/AbocadoProject/components/VacationDetailView.js
+++ Client/AbocadoProject/components/VacationDetailView.js
@@ -1,135 +1,161 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from "react-native"
-import { Card } from 'react-native-elements'
-
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Card } from 'react-native-elements';
 
 export default class VacationDetailView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        data : {
-          annual : 0,
-           
-        },
-      };
+      changedConsolation: [],
+      change : []
+    };
   }
-  
+
   /**************************************************************************/
   /* function which output detail */
 
-  _getAnnual = () => { // Annual TextInput, 값 없으면 에러 안나게. 
-    let annual = []
-    if(this.props.item.annual.length!=0) {
-      annual.push( <TextInput  
-      placeholder={this.props.item.annual[0].day + '일'}
-      placeholderTextColor='black'
-      underlineColorAndroid='transparent'  
-      style={[styles.textInput, styles.dayInput]} 
-      keyboardType={'numeric'}/>)
+  _getAnnual = () => {
+    // Annual TextInput, 값 없으면 에러 안나게.
+    let annual = [];
+    if (this.props.item.annual.length != 0) {
+      annual.push(
+        <TextInput
+          placeholder={this.props.item.annual[0].day + '일'}
+          placeholderTextColor="black"
+          underlineColorAndroid="transparent"
+          style={[styles.textInput, styles.dayInput]}
+          keyboardType={'numeric'}
+        />
+      );
+    } else
+      annual.push(
+        <TextInput
+          placeholder={0 + '일'}
+          placeholderTextColor="black"
+          underlineColorAndroid="transparent"
+          style={[styles.textInput, styles.dayInput]}
+          keyboardType={'numeric'}
+        />
+      );
+
+    return annual;
+  };
+
+  _getConsolation = () => {
+    // consolation TextInput
+    let consolation = [];
+    for (let i = 0; i < this.props.item.consolation.length; i++) {
+      consolation.push(
+        <View style={styles.detailContainer}>
+          <TextInput
+            placeholder={this.props.item.consolation[i].day + '일'}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.dayInput]}
+            keyboardType={'numeric'}
+          />
+          <TextInput
+            placeholder={this.props.item.consolation[i].title}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.titleInput]}
+            onEndEditing={() => {
+              this.state.changedConsolation.push(this.state.change[this.state.change.length-1])
+              console.log(this.state.changedConsolation)
+            }}
+            onChangeText={(text) => {
+              let temp = {
+                title: text,
+              }
+              this.state.change.push(temp)
+            }}
+          />
+        </View>
+      );
     }
-    else annual.push( <TextInput  
-        placeholder={0 + '일'}
-        placeholderTextColor='black'
-        underlineColorAndroid='transparent'  
-        style={[styles.textInput, styles.dayInput]} 
-        keyboardType={'numeric'}/>)
-    
-    return annual
-  } 
+    return consolation;
+  };
 
-  _getConsolation = () => {  // consolation TextInput
-    let consolation = []
-    for(let i=0; i<this.props.item.consolation.length; i++) {
-        consolation.push( 
-          <View style={styles.detailContainer}>
-            <TextInput  
-              placeholder={this.props.item.consolation[i].day + '일'}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.dayInput]} 
-              keyboardType={'numeric'}/>
-            <TextInput  
-              placeholder={this.props.item.consolation[i].title}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.titleInput]}/>
-          </View> 
-      )
+  _getPrize = () => {
+    // Prize TextInput
+    let prize = [];
+    for (let i = 0; i < this.props.item.prize.length; i++) {
+      prize.push(
+        <View style={styles.detailContainer}>
+          <TextInput
+            placeholder={this.props.item.prize[i].day + '일'}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.dayInput]}
+            keyboardType={'numeric'}
+          />
+          <TextInput
+            placeholder={this.props.item.prize[i].title}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.titleInput]}
+          />
+        </View>
+      );
     }
-    return consolation
-  }
+    return prize;
+  };
 
-  _getPrize = () => {  // Prize TextInput
-    let prize = []
-    for(let i=0; i<this.props.item.prize.length; i++) {
-        prize.push( 
-          <View style={styles.detailContainer}>
-            <TextInput  
-              placeholder={this.props.item.prize[i].day + '일'}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.dayInput]} 
-              keyboardType={'numeric'}/>
-            <TextInput  
-              placeholder={this.props.item.prize[i].title}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.titleInput]}/>
-          </View> 
-      )
+  _getReward = () => {
+    // Prize TextInput
+    let reward = [];
+    for (let i = 0; i < this.props.item.reward.length; i++) {
+      reward.push(
+        <View style={styles.detailContainer}>
+          <TextInput
+            placeholder={this.props.item.reward[i].day + '일'}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.dayInput]}
+            keyboardType={'numeric'}
+          />
+          <TextInput
+            placeholder={this.props.item.reward[i].title}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.titleInput]}
+          />
+        </View>
+      );
     }
-    return prize
-  }
+    return reward;
+  };
 
-  _getReward = () => {  // Prize TextInput
-    let reward = []
-    for(let i=0; i<this.props.item.reward.length; i++) {
-        reward.push( 
-          <View style={styles.detailContainer}>
-            <TextInput  
-              placeholder={this.props.item.reward[i].day + '일'}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.dayInput]} 
-              keyboardType={'numeric'}/>
-            <TextInput  
-              placeholder={this.props.item.reward[i].title}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.titleInput]}/>
-          </View> 
-      )
+  _getPetition = () => {
+    // Prize TextInput
+    let petition = [];
+    for (let i = 0; i < this.props.item.petition.length; i++) {
+      petition.push(
+        <View style={styles.detailContainer}>
+          <TextInput
+            placeholder={this.props.item.petition[i].day + '일'}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.dayInput]}
+            keyboardType={'numeric'}
+          />
+          <TextInput
+            placeholder={this.props.item.petition[i].title}
+            placeholderTextColor="black"
+            underlineColorAndroid="transparent"
+            style={[styles.textInput, styles.titleInput]}
+          />
+        </View>
+      );
     }
-    return reward
-  }
+    return petition;
+  };
 
-  _getPetition = () => {  // Prize TextInput
-    let petition = []
-    for(let i=0; i<this.props.item.petition.length; i++) {
-        petition.push( 
-          <View style={styles.detailContainer}>
-            <TextInput  
-              placeholder={this.props.item.petition[i].day + '일'}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.dayInput]} 
-              keyboardType={'numeric'}/>
-            <TextInput  
-              placeholder={this.props.item.petition[i].title}
-              placeholderTextColor='black'
-              underlineColorAndroid='transparent'  
-              style={[styles.textInput, styles.titleInput]}/>
-          </View> 
-      )
-    }
-    return petition
-  }
-
-/**************************************************************************/
-
+  /**************************************************************************/
 
   render() {
-    return ( //DetailView flatlist로 구현하기
+    return (
+      //DetailView flatlist로 구현하기
       <Card containerStyle={styles.container}>
         <View style={styles.category}>
           <Text style={styles.title}>연가</Text>
@@ -152,7 +178,7 @@ export default class VacationDetailView extends Component {
           <View>{this._getPetition()}</View>
         </View>
       </Card>
-    )
+    );
   }
 }
 
@@ -174,23 +200,23 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    paddingBottom: 10, 
+    paddingBottom: 10,
   },
-  detailContainer:{
+  detailContainer: {
     flexDirection: 'row',
   },
   textInput: {
     textAlign: 'center',
     height: 30,
-    borderRadius: 5,  
-    borderWidth: 0.5,  
+    borderRadius: 5,
+    borderWidth: 0.5,
     borderColor: 'gray',
   },
-  dayInput: {  
+  dayInput: {
     width: 40,
   },
-  titleInput: {  
+  titleInput: {
     width: 250,
     marginLeft: 10,
   },
-})
+});
